931d4335486f960d7cb92e86c78b5117b965e9af
Nabil Hassein <nabil.hassein@gmail.com>
2012-12-30 12:51:42 -0500

    nlpwp exercise 3.2.1

--- nlpwp.hs
+++ nlpwp.hs
@@ -1,4 +1,4 @@
-{-# Language NoMonomorphismRestriction #-}
+{-# Language NoMonomorphismRestriction, TupleSections #-}
 
 import qualified Data.Set as Set
 import qualified Data.Map as Map
@@ -21,7 +21,23 @@ countElem m e = Map.insertWith (+) e 1 m
 frequencyList :: Ord k => [k] -> Map.Map k Int
 frequencyList = foldl countElem Map.empty
 
+-- n should be greater than zero
+ngram :: Int -> [a] -> [[a]]
+ngram n xs = if length xs >= n
+             then take n xs : ngram n (tail xs)
+             else []
+
+bigram :: [a] -> [[a]]
+bigram = ngram 2
+
+
+--exercise 3.2.1
+skipBigram :: [a] -> [(a, a)]
+skipBigram []     = []
+skipBigram (x:xs) = map (x,) xs ++ skipBigram xs
+
+
 main :: IO ()
-main = do
-  c <- hGetContents =<< openFile "nlpwp-data/brown.txt" ReadMode
-  putStrLn . show $ Map.lookup "the" (frequencyList (words c))
+main = openFile "nlpwp-data/brown.txt" ReadMode >>= hGetContents >>=
+       putStrLn . show . Map.lookup "the" . frequencyList . words
+
