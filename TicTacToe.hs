78185869d28155a40d6e5152d85c166f87fdc91f
Nabil Hassein <nabil.hassein@gmail.com>
2013-03-22 18:54:48 -0400

    cleanup and bug fixes

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -4,6 +4,7 @@ import qualified Data.Map as Map
 import Safe (readMay)
 
 
+-- data types and their display logic
 data Piece = X | O deriving (Eq, Show)
 
 data Error = NonEmpty | OutOfBounds | BadInput
@@ -15,8 +16,21 @@ instance Show Error where
 type Position = (Int, Int)
 
 type Board = Map.Map Position Piece
+showBoard :: Board -> String
+showBoard board =
+  let spot = showCell . flip Map.lookup board
+      showCell (Just X) = "X"
+      showCell (Just O) = "O"
+      showCell Nothing  = " "
+  in "\n   1   2   3 "                                                      ++ 
+     "\n 1 " ++ spot (1, 1) ++ " | " ++ spot (2, 1) ++ " | " ++ spot (3, 1) ++ 
+     "\n  ---|---|---"                                                      ++
+     "\n 2 " ++ spot (1, 2) ++ " | " ++ spot (2, 2) ++ " | " ++ spot (3, 2) ++
+     "\n  ---|---|---"                                                      ++
+     "\n 3 " ++ spot (1, 3) ++ " | " ++ spot (2, 3) ++ " | " ++ spot (3, 3)
 
 
+-- useful helper functions and variables
 other :: Piece -> Piece
 other X = O
 other O = X
@@ -35,6 +49,24 @@ emptyBoard :: Board
 emptyBoard = Map.empty
 
 
+-- game logic
+win :: Board -> Piece -> Bool
+win board piece = or [threeInARow board piece lane | lane <- winningPositions]
+
+threeInARow :: Board -> Piece -> [Position] -> Bool
+threeInARow board piece lane = all (== (Just piece))
+                               [Map.lookup spot board | spot <- lane]
+
+draw :: Board -> Bool
+draw board = (Map.size board == 9) && not (win board X) && not (win board O)
+
+gameOver :: Board -> Maybe String
+gameOver board
+  | win board X = Just "Player X wins!"
+  | win board O = Just "Player O wins!"
+  | draw board  = Just "Cat's Game."
+  | otherwise   = Nothing
+
 makeMove :: Board -> String -> Piece -> Either Error Board
 makeMove board position piece = let legal = [1, 2, 3] in
   case readMay position :: Maybe (Int, Int) of
@@ -45,47 +77,11 @@ makeMove board position piece = let legal = [1, 2, 3] in
                      Nothing -> Right $ Map.insert (x, y) piece board
                      _       -> Left NonEmpty
 
-
-win :: Board -> Piece -> Bool
-win board pieceType = or [threeInARow board pieceType lane
-                         | lane <- winningPositions]
-
-threeInARow :: Board -> Piece -> [Position] -> Bool
-threeInARow board pieceType lane = 3 == length (filter (== (Just pieceType))
-                                          [Map.lookup spot board | spot <- lane])
-
-draw :: Board -> Bool
-draw board = (Map.size board == 9) && not (win board X) && not (win board O)
-
-gameOver :: Board -> Maybe Piece
-gameOver board
-  | win board X = Just X
-  | win board O = Just O
-  | otherwise   = Nothing
-
-
-
-showCell :: Maybe Piece -> String
-showCell (Just X) = "X"
-showCell (Just O) = "O"
-showCell Nothing  = " "
-
-showBoard :: Board -> String
-showBoard board =
-  let spot = showCell . flip Map.lookup board
-  in "   1   2   3 \n"                                                    ++ 
-     " 1 " ++ spot (1, 1) ++ " | " ++ spot (2, 1) ++ " | " ++ spot (3, 1) ++ 
-     "\n  ---|---|---\n"                                                  ++
-     " 2 " ++ spot (1, 2) ++ " | " ++ spot (2, 2) ++ " | " ++ spot (3, 2) ++
-     "\n  ---|---|---\n"                                                  ++
-     " 3 " ++ spot (1, 3) ++ " | " ++ spot (2, 3) ++ " | " ++ spot (3, 3)
-
-
 loop :: Board -> Piece -> IO ()
 loop board piece = do
   putStrLn $ showBoard board
   case gameOver board of
-    Just winner -> putStrLn $ "Player " ++ show winner ++ " wins!"
+    Just string -> putStrLn string
     Nothing     -> do
       putStrLn $ "\nPlayer " ++ show piece ++ ", move in the form (x, y)."
       input <- getLine
