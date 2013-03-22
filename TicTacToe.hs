2c1ed586ebff082cb60d2d44bdb3efc92212f140
Nabil Hassein <nabil.hassein@gmail.com>
2013-03-22 14:16:41 -0400

    got rid of extraneous MaybeT monad transformer

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -2,9 +2,7 @@ module TicTacToe where
 
 import qualified Data.Map as Map
 import Safe (readMay)
-import Control.Monad (guard, when)
-import Control.Monad.Trans (liftIO)
-import Control.Monad.Maybe (MaybeT, runMaybeT)
+import Control.Monad (when)
 
 data Piece = X | O deriving (Eq, Show)
 
@@ -30,30 +28,28 @@ winningPositions = [[(1, 1), (1, 2), (1, 3)], -- first 3: vertical
 emptyBoard :: Board
 emptyBoard = Map.empty
 
-puts :: String -> MaybeT IO ()
-puts = liftIO . putStrLn
 
-
-makeMove :: Board -> Piece -> MaybeT IO Board
+makeMove :: Board -> Piece -> IO Board
 makeMove board piece = do
-  puts $ "Player " ++ show piece ++ " to move."
+  putStrLn $ "Player " ++ show piece ++ " to move."
   position <- getMove
   case Map.lookup position board of
     Nothing -> return $ Map.insert position piece board
-    _       -> puts "Choose an empty square." >> makeMove board piece
+    _       -> putStrLn "Choose an empty square." >> makeMove board piece
 
 
-getMove :: MaybeT IO Position
+getMove :: IO Position
 getMove = do
-  puts "Make your move in the form (x, y)"
-  input <- liftIO getLine
+  putStrLn "Make your move in the form (x, y)"
+  input <- getLine
   let pos   = readMay input :: Maybe (Int, Int)
       legal = [1, 2, 3]
   case pos of
     Just (x, y) -> if x `elem` legal && y `elem` legal
                    then return (x, y)
-                   else puts "Both coordinates should be from 1 to 3." >> getMove
-    _           -> puts "Couldn't understand input. Try again." >> getMove
+                   else putStrLn "Both coordinates should be from 1 to 3."
+                        >> getMove
+    _           -> putStrLn "Couldn't understand input. Try again." >> getMove
 
 
 win :: Board -> Piece -> Bool
@@ -69,14 +65,14 @@ draw board = full && not (win board X) && not (win board O)
   where full   = length values == 9
         values = map snd $ Map.toList board
 
-gameOver :: Board -> MaybeT IO Bool
+gameOver :: Board -> IO Bool
 gameOver board = do
   let x = win board X
       o = win board O
       d = draw board
-  when x $ puts "Player X wins!"
-  when o $ puts "Player O wins!"
-  when d $ puts "Cat's game!"
+  when x $ putStrLn "Player X wins!"
+  when o $ putStrLn "Player O wins!"
+  when d $ putStrLn "Cat's game!"
   return $ x || o || d
 
 showCell :: Maybe Piece -> String
@@ -95,14 +91,15 @@ showBoard board =
      " 3 " ++ spot (1, 3) ++ " | " ++ spot (2, 3) ++ " | " ++ spot (3, 3) ++
      "\n"
 
-loop :: Board -> Piece -> MaybeT IO ()
+loop :: Board -> Piece -> IO ()
 loop board piece = do
-  puts $ showBoard board
+  putStrLn $ showBoard board
   done <- gameOver board
-  guard $ not done
-  newBoard <- makeMove board piece
-  loop newBoard (other piece)
+  if not done
+    then do newBoard <- makeMove board piece
+            loop newBoard (other piece)
+    else return ()
 
-main :: IO (Maybe ())
-main = runMaybeT $ loop emptyBoard X
+main :: IO ()
+main = loop emptyBoard X
 
