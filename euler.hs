64e33f93d5c62f084f9a68ba7e4019c46e772c2c
Nabil Hassein <nabil.hassein@gmail.com>
2013-07-10 23:03:25 -0400

    Committing old exercise changes, with attempts to comment. First two project Euler problems.

--- euler.hs
+++ euler.hs
@@ -1,9 +1,7 @@
-import Data.List (nub)
-
+ex1 :: Integral a => a
 ex1 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 
+ex2 :: Integral c => c
 ex2 = sum . filter even . takeWhile (< 4000000) $ fibs
   where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-
