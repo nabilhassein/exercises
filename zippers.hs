bc50132cf1a17f5c06292c1ac3bf89d50cffea1e
Nabil Hassein <nabil.hassein@gmail.com>
2013-07-10 23:39:48 -0400

    finished through 'a very simple file system'

--- zippers.hs
+++ zippers.hs
@@ -1,25 +1,40 @@
--- based on http://learnyouahaskell.com/zippers
+-- based loosely on http://learnyouahaskell.com/zippers
 
 module Zippers where
+
+import qualified Data.Text            as T
+import qualified Data.ByteString.Lazy as BL
 
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
 
-data Direction = L | R deriving (Show)
-type Directions = [Direction]
+data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving (Show)
 
--- the obvious generalization of the original code
-alterTree :: a -> Directions -> Tree a -> Tree a
-alterTree a (L:ds) (Node x l r) = Node x (alterTree a ds l) r
-alterTree a (R:ds) (Node x l r) = Node x l (alterTree a ds r)
-alterTree a []     (Node _ l r) = Node a l r
+type Zipper a = (Tree a, [Crumb a])
 
-changeToP' = alterTree 'P'
+(-:) :: a -> (a -> b) -> b
+x -: f = f x
 
-elemAt :: Directions -> Tree a -> a
-elemAt (L:ds) (Node _ l _) = elemAt ds l
-elemAt (R:ds) (Node _ _ r) = elemAt ds r
-elemAt [] (Node x _ _) = x
+goLeft :: Zipper a -> Zipper a
+goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
+
+goRight :: Zipper a -> Zipper a
+goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
+
+goUp :: Zipper a -> Zipper a
+goUp (t, (LeftCrumb  x r) : bs) = (Node x t r, bs)
+goUp (t, (RightCrumb x l) : bs) = (Node x l t, bs)
+
+modify :: (a -> a) -> Zipper a -> Zipper a
+modify f ((Node x l r), bs) = (Node (f x) l r, bs)
+modify _ (Empty,        bs) = (Empty, bs)
+
+attach :: Tree a -> Zipper a -> Zipper a
+attach t (_, bs) = (t, bs)
+
+topMost :: Zipper a -> Zipper a
+topMost (t,[]) = (t,[])
+topMost z      = topMost (goUp z)
 
 
 freeTree :: Tree Char
