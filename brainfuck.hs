43e917c060889c6fe4174cfc83647500f19c5d9e
Nabil Hassein <nabil.hassein@gmail.com>
2013-07-11 02:57:05 -0400

    improved documentation

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,7 +1,7 @@
 module Brainfuck where
 
-import           Data.Char            (ord)
-import           Data.Word            (Word8)
+import Data.Char (ord)
+import Data.Word (Word8)
 
 
 type Zipper a = ([a], a, [a])
@@ -22,27 +22,27 @@ initialMemory :: Memory
 initialMemory = ([], 0, repeat 0)
 
 
--- (>)
+-- (>): increment the data pointer (to point to the next cell to the right)
 incrementDataPointer :: Memory -> Memory
 incrementDataPointer = goRight
 
--- (<)
+-- (<): decrement the data pointer (to point to the next cell to the left)
 decrementDataPointer :: Memory -> Memory
 decrementDataPointer = goLeft
 
--- (+)
+-- (+): increment the byte at the data pointer
 increment :: Memory -> Memory
 increment (ls, x, rs) = (ls, x+1, rs)
 
--- (-)
+-- (-): decrement the byte at the data pointer.
 decrement :: Memory -> Memory
 decrement (ls, x, rs) = (ls, x-1, rs)
 
--- (.)
+-- (.): output the byte at the data pointer.
 output :: Memory -> IO ()
 output (_, x, _) = print x
 
--- (,)
+-- (,): accept 1 byte of input; store its value in the byte at the data pointer
 input :: Memory -> IO Memory
 input (ls, _, rs) = do
   char <- getChar
@@ -50,9 +50,8 @@ input (ls, _, rs) = do
       byte = toEnum $ ord char
   return (ls, byte, rs)
 
--- ([)
--- If the byte at the data pointer (i.e. the focus of the Memory zipper) is zero
--- then this function jumps the instruction pointer (i.e. the focus of the
+-- ([): If the byte at the data pointer (i.e. the focus of the Memory zipper) is
+-- zero, then this function jumps the instruction pointer (i.e. the focus of the
 -- Program zipper) FORWARD to the command after the matching ']' command.
 -- If the byte at the data pointer is nonzero, then this function simply
 -- increments the instruction pointer forward to the command.
@@ -66,13 +65,12 @@ lb program (_, 0, _) = jumpPast ']' program
 
 lb program _         = goRight program
 
--- (])
--- If the byte at the data pointer (i.e. the focus of the Memory zipper) is zero
--- then this function simply increments the instruction pointer (i.e. the focus
--- of the Program zipper).
+-- (]): If the byte at the data pointer (i.e. the focus of the Memory zipper) is
+-- zero,then this function simply increments the instruction pointer (i.e. the
+-- focus of the Program zipper).
 -- If the byte at the data pointer is nonzero then instead of moving the
 -- instruction pointer forward to the next command, this function jumps the
--- instruction pointer BACK to the command after the matching '[' command
+-- instruction pointer BACK to the command after the matching '[' command.
 rb :: Program -> Memory -> Program
 rb program (_, 0, _) = goRight program
 rb program _         = jumpBackTo '[' program
