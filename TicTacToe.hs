b5e51b812f1cb133ef817963901b2e5588833440
Nabil Hassein <nabil.hassein@gmail.com>
2013-07-10 23:08:47 -0400

    Committing old exercise changes, with attempts to comment. Many changes to tictactoe.

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -5,10 +5,11 @@ module Main where
 import           Safe (readMay)
 import qualified Data.Map as Map
 
--- data types and their display logic
-data Piece = X | O deriving (Eq, Show, Read)
 
-data Error = NonEmptySquare | OutOfBounds | BadInput
+-- data types and their display logic
+data Piece = X | O deriving (Eq, Show)
+
+data Error = BadInput | OutOfBounds | NonEmptySquare
 instance Show Error where
   show NonEmptySquare = "\nThat square is already taken. Try another move."
   show OutOfBounds    = "\nOut of bounds. Choose a square from 1 to 3."
@@ -19,9 +20,8 @@ type Position = (Int, Int)
 type Board = Map.Map Position Piece
 showBoard :: Board -> String
 showBoard board = let showCell :: Maybe Piece -> String
-                      showCell (Just X) = "X"
-                      showCell (Just O) = "O"
-                      showCell Nothing  = " "
+                      showCell (Just piece) = show piece
+                      showCell Nothing      = " "
 
                       spot :: Position -> String
                       spot = showCell . flip Map.lookup board
@@ -30,20 +30,21 @@ showBoard board = let showCell :: Maybe Piece -> String
      "\n  ---|---|---"                                                      ++
      "\n 2 " ++ spot (1, 2) ++ " | " ++ spot (2, 2) ++ " | " ++ spot (3, 2) ++
      "\n  ---|---|---"                                                      ++
-     "\n 3 " ++ spot (1, 3) ++ " | " ++ spot (2, 3) ++ " | " ++ spot (3, 3)
+     "\n 3 " ++ spot (1, 3) ++ " | " ++ spot (2, 3) ++ " | " ++ spot (3, 3) ++
+     "\n\n"
 
 -- useful helper functions and variables
 other :: Piece -> Piece
 other X = O
 other O = X
 
-legal :: [Int]
+--legal :: [Position]
 legal = [1, 2, 3]
 
 emptyBoard :: Board
 emptyBoard = Map.empty
 
-winningPositions :: [[Position]]
+--winningPositions :: [(Position, Position, Position)]
 winningPositions = [map (1,) legal, -- first three are vertical
                     map (2,) legal,
                     map (3,) legal,
@@ -53,16 +54,18 @@ winningPositions = [map (1,) legal, -- first three are vertical
                     map (\x -> (x, x)) legal, -- last two are diagonal
                     [(x, y) | x <- legal, y <- legal, x + y == 4]]
 
+
 -- game logic
+draw :: Board -> Bool
+draw board = Map.size board == 9 && not (win board X) && not (win board O)
+
 win :: Board -> Piece -> Bool
 win board piece = any (threeInARow board piece) winningPositions
 
-threeInARow :: Board -> Piece -> [Position] -> Bool
-threeInARow board piece lane = all (== Just piece) [Map.lookup spot board
-                                                   | spot <- lane]
-
-draw :: Board -> Bool
-draw board = Map.size board == 9 && not (win board X) && not (win board O)
+-- retest this after implementing winningPositions
+threeInARow :: Board -> Piece -> (Position, Position, Position) -> Bool
+threeInARow board piece (x, y, z) = all (== Just piece) [Map.lookup spot board
+                                                        | spot <- [x, y, z]]
 
 gameOver :: Board -> Maybe String
 gameOver board
@@ -71,29 +74,29 @@ gameOver board
   | draw board  = Just "\nCat's Game."
   | otherwise   = Nothing
 
-makeMove :: Board -> Piece -> Maybe Position -> Either Error Board
-makeMove _      _    Nothing       = Left BadInput
-makeMove board piece (Just (x, y)) =
-  if x `notElem` legal || y `notElem` legal
-  then Left OutOfBounds
-  else case Map.lookup (x, y) board of
-    Just _  -> Left NonEmptySquare
-    Nothing -> Right $ Map.insert (x, y) piece board
+makeMove :: Position -> Piece -> Board -> Board
+makeMove = Map.insert
 
--- game setup and execution; only these functions perform I/O
+parseMove :: String -> Board -> Either Error Position
+parseMove input board = case readMay input of
+  Nothing       -> Left BadInput
+  Just position -> if position `notElem` legal
+                   then Left OutOfBounds
+                   else case Map.lookup position board of
+                     Just _  -> Left NonEmptySquare
+                     Nothing -> Right position
+
+-- game setup and execution
 main :: IO ()
 main = loop emptyBoard X
 
 loop :: Board -> Piece -> IO ()
-loop board piece = do
-  putStrLn $ showBoard board
-  case gameOver board of
-    Just string -> putStrLn string
-    Nothing     -> do
-      putStrLn $ "\nPlayer " ++ show piece ++ ", move in the form (x, y)."
-      input <- getLine
-      let position = readMay input :: Maybe Position
-      case makeMove board piece position of
-        Left  err      -> print err >> loop board piece
-        Right newBoard -> loop newBoard (other piece)
-
+loop board piece = case gameOver board of
+  Just string -> putStrLn string
+  Nothing     -> do
+    putStrLn $ showBoard board ++ show piece ++ ", move in the form (x, y)."
+    input <- getLine
+    case parseMove input board of
+      Left  err      -> print err >> loop board piece
+      Right position -> let newBoard = makeMove position piece board
+                        in loop newBoard (other piece)
