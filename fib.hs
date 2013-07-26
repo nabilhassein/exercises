a683609a7ceb1d5ea101ea3d126967447ba28162
Nabil Hassein <nabil.hassein@gmail.com>
2013-07-26 01:22:19 -0400

    simple memoized fibonnaci function

---
+++ fib.hs
@@ -0,0 +1,11 @@
+-- basically copied and pasted from:
+-- http://www.haskell.org/haskellwiki/Memoization#Memoization_with_recursion
+
+memoized_fib :: Int -> Integer
+memoized_fib    n    = map fib [0 ..] !! n
+   where fib    0    = 0
+         fib    1    = 1
+         fib    k    = memoized_fib (k-2) + memoized_fib (k-1)
+
+main :: IO ()
+main = mapM_ print $ map memoized_fib [1..50]
